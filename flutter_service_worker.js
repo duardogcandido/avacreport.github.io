dab57fb905653ace1ab19117b758db22b013cfbb
Eduardo <eduardo.gcandido@outlook.com>
2023-09-14 15:19:57 -0300

    adicionada tela de politica de privacidade

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -1401,9 +1401,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "f94a5b2e73d7e2653e96864253f0ee91
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "0ec4fcde5b74a3063138789487415add",
-"/": "0ec4fcde5b74a3063138789487415add",
-"main.dart.js": "2a0dcb225bec9e56030a527bc034b305",
+"index.html": "16851be591a3ce900c3cfeb267bc540c",
+"/": "16851be591a3ce900c3cfeb267bc540c",
+"main.dart.js": "f0e4b620c1851aa8543cb926647b7a23",
 "manifest.json": "77dabf72449ca7032f7ecd2f46b88502",
 "version.json": "f4947d9e9d98f323167033cb9a47de3f"};
 // The application shell files that are downloaded before a service worker can
