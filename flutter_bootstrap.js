3449b79a65ab5d6dc2d02eef7a4145e891b44063
Eduardo <eduardo.gcandido@outlook.com>
2026-09-01 08:39:09 -0300

    fix

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"4c525dac5ebe5971c5708ef73558ed8edcf4a3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2201706597" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "234808578" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
